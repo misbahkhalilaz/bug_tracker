db5db93c5d384448f6ba146a55635697a99b7063
Misbah Khalil <misbah.khalil.az@gmail.com>
2020-04-28 12:17:56 +0500

    'better_design_pattern'

---
+++ src/redux/reducers/splittedReducers.js
@@ -0,0 +1,34 @@
+import { CHANGE_FILTER, visibilityFilters } from "../actionTypes";
+import * as actions from "../actionTypes";
+let lastId = 0;
+
+export function visibilityFilter(state = visibilityFilters.SHOW_ALL, action) {
+	if (action.type === CHANGE_FILTER) return action.payload.visibilityFilter;
+	else return state;
+}
+
+export function bugs(state = [], action) {
+	switch (action.type) {
+		case actions.BUG_ADDED:
+			if (action.payload.description === "") return state;
+			return [
+				...state,
+				{
+					id: ++lastId,
+					description: action.payload.description,
+					resolved: false,
+				},
+			];
+
+		case actions.BUG_REMOVED:
+			return state.filter((bug) => bug.id !== action.payload.id);
+
+		case actions.BUG_RESOLVED:
+			return state.map((bug) =>
+				bug.id !== action.payload.id ? bug : { ...bug, resolved: !bug.resolved }
+			);
+
+		default:
+			return state;
+	}
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,19 @@
 import { createStore } from "redux";
 import reducer from "./reducers/reducer";
+import { visibilityFilters } from "./actionTypes";
 
-const store = createStore(reducer); //store is created by giving the designed reducer, now its API is usable
+const initState = {
+	//this is the final shape of our states in store
+	visibilityFilter: visibilityFilters.SHOW_ALL,
+	bugs: [
+		// {
+		// 	id: null,
+		// 	description: "",
+		// 	resolved: false,
+		// },
+	],
+};
+
+const store = createStore(reducer, initState); //store is created by giving the designed reducer, now its API is usable
 
 export default store;
